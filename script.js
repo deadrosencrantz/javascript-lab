ac2ae773fdd9c8fb86743d697339c1bc9a702b70
R <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-14 14:54:49 +0100

    Understand .pop

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const pets = ['cat', 'dog', 'rabbit', 'hamster', 'goldfish', 'pigeon']
 
-pets.push('guinea pig');
+pets.push('guinea pig'); // add to end of list
 
-console.log(pets) //outputs "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon", "guinea pig"
+console.log(pets) // "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon", "guinea pig"
+
+console.log(pets.pop()) // "guinea pig"
+
+console.log(pets) // "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon" 
+
