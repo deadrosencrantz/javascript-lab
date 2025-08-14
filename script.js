803eb131083f7f25b50c06f960c2ede58bb6595a
R <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-14 14:45:22 +0100

    Understand .push

---
+++ script.js
@@ -0,0 +1,5 @@
+const pets = ['cat', 'dog', 'rabbit', 'hamster', 'goldfish', 'pigeon']
+
+pets.push('guinea pig');
+
+console.log(pets) //outputs "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon", "guinea pig"
