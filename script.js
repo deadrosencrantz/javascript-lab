4a364226d6da73ef1c381c272c73873d17086e03
R <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-14 15:02:38 +0100

    Understand .shift

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const pets = ['cat', 'dog', 'rabbit', 'hamster', 'goldfish', 'pigeon']
+const pets = ['cat', 'dog', 'rabbit', 'hamster', 'goldfish', 'pigeon'];
 
 pets.push('guinea pig'); // add to end of list
 
-console.log(pets) // "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon", "guinea pig"
+console.log(pets); // "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon", "guinea pig"
 
-console.log(pets.pop()) // "guinea pig"
+console.log(pets.pop()); //removes and returns last element, guinea pig
 
-console.log(pets) // "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon" 
+console.log(pets); //returns "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon" 
 
+console.log(pets.shift()); //removes and returns first element, cat
+
+console.log(pets); //returns "dog", "rabbit", "hamster", "goldfish", "pigeon"
