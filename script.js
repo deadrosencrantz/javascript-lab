a539706da43400a91adeed804f2de43ce1524a03
R <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-14 15:08:06 +0100

    UNderstand .unshift

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ console.log(pets); //returns "cat", "dog", "rabbit", "hamster", "goldfish", "pig
 console.log(pets.shift()); //removes and returns first element, cat
 
 console.log(pets); //returns "dog", "rabbit", "hamster", "goldfish", "pigeon"
+
+pets.unshift('cat'); //add to start of list
+
+console.log(pets); //returns "cat", "dog", "rabbit", "hamster", "goldfish", "pigeon"
